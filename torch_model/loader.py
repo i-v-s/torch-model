6f8559c80e9a9418cc6b5cd5f767e313e9e11b8f
Igor Smirnov <smirnoviv@rambler.ru>
2019-12-25 23:43:10 +0500

    Add add, maxpool2d, nop

--- torch_model/loader.py
+++ torch_model/loader.py
@@ -33,11 +33,44 @@ class Permute(nn.Module):
         return x.permute(self.dims)
 
 
+class Add(nn.Module):
+    def __init__(self, *modules):
+        super().__init__()
+        self.items = nn.ModuleList(modules)
+
+    def forward(self, *args, **kwargs):
+        result = self.items[0](*args, **kwargs)
+        for item in self.items[1:]:
+            result += item(*args, **kwargs)
+        return result
+
+
+def add_shape_calc(shapes):
+    result = shapes[0]
+    for shape in shapes[1:]:
+        assert result == shape
+    return result
+
+
 def permute(shape, *dims, batch_dims=1, loader_info=None):
     assert len(shape) == len(dims) + batch_dims
     dims = tuple(range(batch_dims)) + tuple(d + batch_dims for d in dims)
     shape = tuple(shape[d] for d in dims)
     return shape, Permute(dims)
+
+
+def el(f, i):
+    return f if type(f) is int else f[i]
+
+
+def maxpool2d(shape, *args, loader_info=None, **kwargs):
+    n, c, h, w = shape
+    pool = nn.MaxPool2d(*args, **kwargs)
+    h, w = (
+        (v + 2 * el(pool.padding, i) - el(pool.dilation, i) * (el(pool.kernel_size, i) - 1) - 1) // el(pool.stride, i) + 1
+        for i, v in enumerate((h, w))
+    )
+    return (n, c, h, w), pool
 
 
 def conv2d(shape, out_channels, *args, loader_info=None, **kwargs):
@@ -82,6 +115,10 @@ def simple(module):
     return func
 
 
+def nop(shape, *args, loader_info=None, **kwargs):
+    return shape, nn.Identity(*args, **kwargs)
+
+
 def bn2d(shape, *args, loader_info=None, **kwargs):
     assert len(shape) == 4
     return shape, nn.BatchNorm2d(shape[1], *args, **kwargs)
@@ -113,38 +150,58 @@ def print_item(indent, name, shape, args=None):
     print(f'{title:50} {shape}')
 
 
-def sequential(shape, items, loader_info: LoaderInfo, seq_name=''):
-    modules, values, verbose, indent = loader_info
-    if verbose:
-        print_item(indent, 'sequence ' + seq_name, shape)
-    indent += '  '
-    loader_info = LoaderInfo(modules, values, verbose, indent)
-    result = []
-    for item in items:
-        if type(item) is str:
-            old_shape = shape
-            shape, model = loader_info.modules[item](shape)
-            if verbose:
-                print_item(indent, item, shape if old_shape != shape else '')
-            result.append(model)
-        else:
-            for name, params in item.items():
-                args, kwargs = parse_params(params, values)
-                shape, model = modules[name](shape, *args, **kwargs, loader_info=loader_info)
+def container(module, module_name, shape_calc=None):
+    def wrapper(shape, items, loader_info: LoaderInfo, seq_name=''):
+        modules, values, verbose, indent = loader_info
+        if verbose:
+            print_item(indent, f'{module_name} {seq_name}', shape)
+        indent += '  '
+        loader_info = LoaderInfo(modules, values, verbose, indent)
+        result, shapes = [], []
+        for item in items:
+            if type(item) is str:
+                new_shape, model = loader_info.modules[item](shape)
                 if verbose:
-                    print_item(indent, name, shape, args)
-                    # title = f"{indent}{name}({', '.join(map(str, args))}):"
-                    # print(f"{title:50} {shape}")
+                    print_item(indent, item, shape if new_shape != shape else '')
                 result.append(model)
-    return shape, nn.Sequential(*result)
+                if shape_calc:
+                    shapes.append(new_shape)
+                else:
+                    shape = new_shape
+            elif type(item) == dict and next(iter(item.keys())) in ['seq', 'add']:
+                item = list(item.items())
+                assert len(item) == 1
+                key, sub_items = item[0]
+                new_shape, model = modules[key](shape, sub_items, loader_info)
+                result.append(model)
+                if shape_calc:
+                    shapes.append(new_shape)
+                else:
+                    shape = new_shape
+            else:
+                for name, params in item.items():
+                    args, kwargs = parse_params(params, values)
+                    new_shape, model = modules[name](shape, *args, **kwargs, loader_info=loader_info)
+                    if verbose:
+                        print_item(indent, name, new_shape, args)
+                        # title = f"{indent}{name}({', '.join(map(str, args))}):"
+                        # print(f"{title:50} {shape}")
+                    result.append(model)
+                    if shape_calc:
+                        shapes.append(new_shape)
+                    else:
+                        shape = new_shape
+        return shape_calc(shapes) if shape_calc else shape, module(*result)
+    return wrapper
 
 
 global_modules = {
-    'conv2d': conv2d, 'convt2d': convT2d, 'conv': Conv, 'seq': sequential,
+    'conv2d': conv2d, 'convt2d': convT2d, 'conv': Conv, 'maxpool2d': maxpool2d,
     'flatten': flatten, 'reshape': reshape, 'permute': permute,
     'linear': linear,
     'relu': simple(nn.ReLU), 'prelu': simple(nn.PReLU), 'lrelu': simple(nn.LeakyReLU), 'sigmoid': simple(nn.Sigmoid),
-    'dropout': simple(nn.Dropout), 'bn2d': bn2d
+    'dropout': simple(nn.Dropout), 'bn2d': bn2d,
+    'nop': nop, 'add': container(Add, 'add', add_shape_calc), 'seq': container(nn.Sequential, 'seq'),
 }
 
 
@@ -154,11 +211,13 @@ def module_dec(name, module):
         modules, values, verbose, indent = loader_info
         values = copy(values)
         values.update({k: v for k, v in zip(params, args)})
-        return modules['seq'](
-            shape, module['seq'],
-            LoaderInfo(modules, values, verbose, indent),
-            seq_name=name
-        )
+        for key in ['seq', 'add']:
+            if key in module:
+                return modules[key](
+                    shape, module[key],
+                    LoaderInfo(modules, values, verbose, indent),
+                    seq_name=name
+                )
     return wrapper
 
 
