4a5606f895a242899fcc7d62e517e4ef2e22cc3f
Igor Smirnov <smirnoviv@rambler.ru>
2020-02-24 21:07:30 +0500

    Change aug and post_proc parameters

--- torch_model/data/parallel.py
+++ torch_model/data/parallel.py
@@ -11,7 +11,6 @@ class ParallelDataset(Dataset):
     def __init__(self,
                  root_dir='data',
                  classes=('images', 'masks'),
-                 aug_classes=('images', 'masks'),
                  aug=None,
                  extensions=('.png', '.jpeg'),
                  loaders: Optional[Dict[str, Any]] = None,
@@ -21,11 +20,10 @@ class ParallelDataset(Dataset):
         self.root_dir = root_dir
         self.loaders = loaders or {}
         self.classes = classes
-        self.aug = aug
-        self.aug_classes = aug_classes
+        self.aug = aug or []
         self.items = []
         self.extensions = extensions
-        self.post_proc = post_proc or None
+        self.post_proc = post_proc
 
     def find_item(self, fn):
         fn = splitext(fn)[0]
@@ -53,18 +51,22 @@ class ParallelDataset(Dataset):
             self.loaders.get(cl, cv2.imread)(fn)
             for cl, fn in zip(self.classes, self.items[item])
         ]
-        if self.aug:
-            image_idx = self.classes.index(self.aug_classes[0])
-            try:
-                mask_idx = self.classes.index(self.aug_classes[1])
-            except ValueError:
-                mask_idx = None
-            augmented = self.aug(image=items[image_idx], mask=items[mask_idx] if mask_idx is not None else None)
+        for aug in self.aug:
+            aug, image, mask = aug if len(aug) == 3 else (aug + (None,))
+            image_idx = self.classes.index(image)
+            mask_idx = None if mask is None else self.classes.index(mask)
+            augmented = aug(
+                image=items[image_idx],
+                mask=items[mask_idx] if mask_idx is not None else None
+            )
             items[image_idx] = augmented['image']
             if mask_idx is not None:
                 items[mask_idx] = augmented['mask']
-        if self.post_proc:
-            for name, proc in self.post_proc.items():
-                idx = self.classes.index(name)
-                items[idx] = proc(items[idx])
+        if self.post_proc is not None:
+            if type(self.post_proc) is list:
+                for name, proc in self.post_proc.items():
+                    idx = self.classes.index(name)
+                    items[idx] = proc(items[idx])
+            else:
+                items = self.post_proc(*items)
         return tuple(items)
