6d443e249152cf80fa055b3a6d15f2f8eddfd624
Igor Smirnov <smirnoviv@rambler.ru>
2021-07-28 18:46:09 +0500

    Add args to Annotation

--- torch_model/anno/annotation.py
+++ torch_model/anno/annotation.py
@@ -7,17 +7,17 @@ from torch_model import load_model
 
 class Annotation:
     """Object of this class consists of annotation data and interface methods."""
-    def __init__(self, model_name: Optional[str] = None, device=None):
+    def __init__(self, model_name: Optional[str] = None, device=None, *args, **kwargs):
         self.model_name = model_name
         self.device = device
         self.model = None
         self.player = None
         self.initial_image = True
         if model_name is not None:
-            self.update_model()
+            self.update_model(*args, **kwargs)
 
-    def update_model(self):
-        self.model = load_model(self.model_name, device=self.device)
+    def update_model(self, *args, **kwargs):
+        self.model = load_model(self.model_name, device=self.device, *args, **kwargs)
 
     def process(self, frame: np.ndarray):
         raise NotImplementedError
